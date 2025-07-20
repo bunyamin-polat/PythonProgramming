43b71b325306c95f7ad282695131c80c00be5c5a
bunyamin-polat <muhendisbp@gmail.com>
2025-07-20 15:34:23 +0200

    Add class file

---
+++ Practice/common.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def print_LL(head):
+    temp = head # Assign head to temp to avoid changing head
+    while temp is not None:
+        print(temp.data, end = ' -> ')
+        temp = temp.next
+    print("None")
+
+def length_LL(head):
+    temp = head # Assign head to temp to avoid changing head
+    count = 0
+    while temp is not None:
+        count += 1
+        temp = temp.next   
+    return count
+
+def take_input_better():
+    value = int(input('Enter the value of the node: '))
+    head = None
+    tail = None
+    
+    while value != -1:
+        new_node = Node(value)
+        if head is None:
+            head = new_node
+            tail = new_node
+        else:
+            tail.next = new_node
+            tail = new_node
+            
+        value = int(input('Enter the value of the node: '))
+        
+    return head
+
+def create_ll_from_list(values):
+    head = None
+    tail = None
+    
+    for value in values:
+        new_node = Node(value)
+        if head is None:
+            head = new_node
+            tail = new_node
+        else:
+            tail.next = new_node # Link the new node to the end of the list
+            tail = new_node # Update tail to the new node
+            
+    return head
